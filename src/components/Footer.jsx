d0617e0da7563e521b9bcb944b763aaea796a5e5
mabdullah525 <abdullahbinabdullah005@gmail.com>
2025-08-28 07:50:06 -0700

    Add social links

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,17 +5,17 @@ const Footer = () => {
   return (
     <footer className="w-full py-4 bg-black-100/55 text-white flex justify-center gap-6">
       <a
-        href="https://www.instagram.com/huzaifahmedz/"
+        href="https://www.instagram.com/m_abdullah525?igsh=enJ4dXZrZzEzeThr&utm_source=ig_contact_invite "
         target="_blank"
         rel="noopener noreferrer"
       >
         <FaInstagram size={24} />
       </a>
-      <a href="https://twitter.comhttps://x.com/HuzaifDev" target="_blank" rel="noopener noreferrer">
+      <a href="https://twitter.comhttps://x.com/Abdullah" target="_blank" rel="noopener noreferrer">
         <FaXTwitter size={24} />
       </a>
       <a
-        href="https://www.linkedin.com/in/huzaif-ahmed-6abb49273/"
+        href="https://www.linkedin.com/in/muhammad-abdullah-04b163289?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app "
         target="_blank"
         rel="noopener noreferrer"
       >
